34a4c2ac293bf475b7a361444b0867bc029dba85
Nicholas Werner <ncwerner@gmail.com>
2020-11-07 13:07:16 -0700

    added plan to the js file

--- script.js
+++ script.js
@@ -15,3 +15,11 @@
 4. Use other APIs to populate info for each button.
 
 */
+
+// user enters a city name - then map is loaded on the screen of the destination, lets look into google API. try to have it auto fill Suggestions for the city.
+
+// weather button - we will need to link the weather API to the button and grab the city name from the search info to display weather 
+
+// Restaurant button - we will need to link the Restaurant  API to the button and grab the city name from the search info to display Restaurant info
+
+// hotel button - we will need to link the hotel  API to the button and grab the city name from the search info to display hotel info
