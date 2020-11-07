5beed395d2db42412d187f10f65ff3311167f5e0
paolorafaelyap <paolorafaelyap@gmail.com>
2020-11-07 13:31:06 -0800

    buttons can launch modals

--- script.js
+++ script.js
@@ -15,3 +15,56 @@
 4. Use other APIs to populate info for each button.
 
 */
+
+/*$(document).ready(function(){
+    $("#food").click(function()
+    {
+        $("#restaurant").modal('show')
+    });
+});
+*/
+//launches restaurant modal
+document.querySelector('#food').addEventListener('click', function(event) {
+    event.preventDefault();
+    var modal = document.querySelector('#restaurant');  // assuming you have only 1
+    var html = document.querySelector('html');
+    modal.classList.add('is-active');
+    html.classList.add('is-clipped');
+  
+    modal.querySelector('.modal-background').addEventListener('click', function(e) {
+      e.preventDefault();
+      modal.classList.remove('is-active');
+      html.classList.remove('is-clipped');
+    });
+  });
+
+ //launches hotel modal 
+  document.querySelector('#places').addEventListener('click', function(event) {
+    event.preventDefault();
+    var modal = document.querySelector('#hotels');  // assuming you have only 1
+    var html = document.querySelector('html');
+    modal.classList.add('is-active');
+    html.classList.add('is-clipped');
+  
+    modal.querySelector('.modal-background').addEventListener('click', function(e) {
+      e.preventDefault();
+      modal.classList.remove('is-active');
+      html.classList.remove('is-clipped');
+    });
+  });
+
+  //launches weather modal 
+  document.querySelector('#temp').addEventListener('click', function(event) {
+    event.preventDefault();
+    var modal = document.querySelector('#weather');  // assuming you have only 1
+    var html = document.querySelector('html');
+    modal.classList.add('is-active');
+    html.classList.add('is-clipped');
+  
+    modal.querySelector('.modal-background').addEventListener('click', function(e) {
+      e.preventDefault();
+      modal.classList.remove('is-active');
+      html.classList.remove('is-clipped');
+    });
+  });
+
